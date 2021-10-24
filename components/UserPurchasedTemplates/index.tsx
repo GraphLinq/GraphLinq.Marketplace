be326828d94bcf24bd147c1a3dbdd23cbb750170
fafifox <fafifox@protonmail.com>
2021-10-24 16:22:28 +0200

    Add UserPurchasedTemplates component

---
+++ components/UserPurchasedTemplates/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import TemplateCard from '../TemplateCard'
+import { Templates } from 'pages'
+import useSWR from 'swr'
+import { Flex, Text } from '@chakra-ui/react'
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
+
+interface UserTemplatesProps {
+  userId: number
+}
+
+const UserPurchasedTemplates: React.FC<UserTemplatesProps> = ({ userId }) => {
+  const { data, error } = useSWR(
+    userId
+      ? `${process.env.NEXT_PUBLIC_MANAGER_URL}/users/${userId}/templates/purchased`
+      : null,
+    userId ? fetcher : null
+  )
+  if (error) return <Text>Failure: Can&apos;t reach API</Text>
+  return (
+    <Flex
+      flexDir="row"
+      wrap="wrap"
+      flex="0 1 auto"
+      justifyContent="start"
+      pt="2rem"
+    >
+      {!data
+        ? 'loading...'
+        : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          data.templates.publishedTemplates.map((t: Templates, i: number) => {
+            return (
+              <TemplateCard
+                key={`${t.id}-${i}`}
+                template={t}
+                user={{
+                  ...data.templates,
+                }}
+              />
+            )
+          })}
+    </Flex>
+  )
+}
+
+export default UserPurchasedTemplates
